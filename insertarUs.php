11a62d38614120fb6b82188a45e11b6cbfe2df65
leoZT <leo_zt611@hotmail.com>
2013-10-03 11:05:34 -0500

    Nuevo Usuario
    Esta es la vista con su lógica para registrar un nuevo usuario

--- insertarUs.php
+++ insertarUs.php
@@ -1,22 +1,25 @@
-<?php
+         <?php
    
                
     include("conexion.php");
-     
-   if(isset($_REQUEST['name']) && !empty($_REQUEST['name'])&&     
 
-    isset($_REQUEST['pass']) && !empty($_REQUEST['pass'])&&
-    isset($_REQUEST['username']) && !empty($_REQUEST['username'])&&
-    isset($_REQUEST['select1']) && !empty($_REQUEST['select1'])) 
+   if(isset($_REQUEST['Name']) && !empty($_REQUEST['Name'])&&     
+
+    isset($_REQUEST['Pass']) && !empty($_REQUEST['Pass'])&&   
+
+    isset($_REQUEST['UserName']) && !empty($_REQUEST['UserName'])&&     
+
+    isset($_REQUEST['rol']) && !empty($_REQUEST['rol'])) 
     {
-      $conexion=mysql_connect($host,$user,$pw)or die("problemas al conectar");
-      mysql_select_db($db,$conexion)or die("problemas al conectar la bd");
-
-      $roles=$_POST['select1'];
+      $con=mysql_connect($host,$user,$pw)or die("problemas al conectar");
+      mysql_select_db($db,$con)or die("problemas al conectar la bd");
+      
+      
+ $roles=$_POST['rol'];
 
    
       mysql_query("INSERT INTO usuarios(nombre,contrasenia,usuario,roles_id) 
-VALUES ('$_POST[name]','$_POST[pass]','$_POST[username]','$roles')",$conexion);
+VALUES ('$_REQUEST[Name]','$_REQUEST[Pass]','$_REQUEST[UserName]','$roles')",$con);
 Include("newuser.php");  
      echo "<h1> DATOS GUARDADOS EXITOSAMENTE...!!</h1>";
    }
